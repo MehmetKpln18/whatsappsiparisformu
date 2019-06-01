9ac16ccf46fc905acc9cefc0d22726dd697ccf97
MehmetKpln18 <mehmetkpln18@gmail.com>
2019-06-01 16:12:16 +0300

    Update veritabanı.sql

--- veritabanı.sql
+++ veritabanı.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: localhost:3306
--- Üretim Zamanı: 20 May 2019, 23:45:29
+-- Üretim Zamanı: 01 Haz 2019, 16:09:24
 -- Sunucu sürümü: 10.0.38-MariaDB
 -- PHP Sürümü: 7.2.7
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Veritabanı: `mehmetk6_siparisformu`
+-- Veritabanı: `siparisformuv1`
 --
 
 -- --------------------------------------------------------
@@ -54,7 +54,7 @@ CREATE TABLE `ayarlar` (
 --
 
 INSERT INTO `ayarlar` (`id`, `site_title`, `site_favicon`, `site_slogan`, `site_description`, `site_keywords`, `site_copright`, `site_facebook`, `site_twitter`, `site_instagram`, `site_analytics`, `site_telefon`, `site_fax`, `site_calsaat`, `site_eposta`, `site_adres`, `site_gorunum`, `updatedAt`) VALUES
-(1, 'PHP | Whatsapp Sipariş Formu Scripti (Beta v.1) - Yönetim Panelli', 'favicon.png', 'Sipariş Formu (Beta v.1)', 'Sipariş Formu (Beta v.1)', 'sipariş formu, mehmet kaplan', 'Copyright © 2019 <a target=\"_blank\" title=\"Mehmet Kaplan\" href=\"https://www.mehmetkaplan.net/\">Mehmet Kaplan</a> tarafından kodlanmıştır.', 'https://www.facebook.com/mehmetkplnn', 'https://www.twitter.com/mehmetkpln18', 'Mehmet Kaplan', '', '0212 212 12 12', '0212 212 12 12', 'Pazartesi - Cumartesi 09:00 - 19:00', 'deneme@deneme.com', 'Adres Buraya gelecek.', '1', '2018-07-28 07:45:48');
+(1, 'PHP | Whatsapp Sipariş Formu Scripti (Beta v.1) - Yönetim Panelli', 'favicon.png', 'Sipariş Formu (Beta v.1)', 'Sipariş Formu (Beta v.1)', 'sipariş formu, mehmet kaplan', 'Copyright © 2019 <a target=\"_blank\" title=\"Mehmet Kaplan\" href=\"https://www.mehmetkaplan.net/\">Mehmet Kaplan</a> tarafından kodlanmıştır.', 'https://www.facebook.com/mehmetkplnn', 'https://www.twitter.com/mehmetkpln18', 'Mehmet Kaplan', '', '0212 212 12 12', '0212 212 12 12', 'Pazartesi - Cumartesi 09:00 - 19:00', 'deneme@deneme.com', 'Eyüpsultan Mah. Günce Sk. No:44 Kemer Apartmanı D:22 B Blok Eyüpsultan - İstanbul', '1', '2018-07-28 07:45:48');
 
 -- --------------------------------------------------------
 
@@ -79,8 +79,8 @@ CREATE TABLE `destektalepleri` (
 --
 
 INSERT INTO `destektalepleri` (`id`, `alanid`, `baslik`, `isim`, `eposta`, `kullanicicevap`, `yoneticicevap`, `durum`, `tarih`) VALUES
-(27, 620, 'Merhaba Destek Sağlarmısınız.', 'Mehmet Kaplan', 'örnek@deneme.com', 'Merhaba Nasıl Sipariş Vereceğim.', 'Destek Sağlandı', '2', '2019-04-11 08:48:07'),
-(28, 234, 'Merhaba Destek Sağlarmısınız.', 'Mehmet Kaplan', 'örnek@deneme.com', 'Merhaba Nasıl Sipariş Vereceğim.', 'Yonetici Mesaji Bekleniyor !', '1', '2019-04-12 06:17:00');
+(1, 620, 'Merhaba Destek Sağlarmısınız.', 'Mehmet Kaplan', 'örnek@deneme.com', 'Merhaba Nasıl Sipariş Vereceğim.', 'Destek Sağlandı', '2', '2019-04-11 08:48:07'),
+(2, 234, 'Merhaba Destek Sağlarmısınız.', 'Mehmet Kaplan', 'örnek@deneme.com', 'Merhaba Nasıl Sipariş Vereceğim.', 'Yonetici Mesaji Bekleniyor !', '1', '2019-04-12 06:17:00');
 
 -- --------------------------------------------------------
 
@@ -101,7 +101,7 @@ CREATE TABLE `duyurular` (
 --
 
 INSERT INTO `duyurular` (`id`, `baslik`, `icerik`, `tarih`, `yazar`) VALUES
-(7, 'Mehmet Kaplan', 'Sipariş Formu Duyuru Alanı', '2019-04-10 10:38:52', 'Mehmet');
+(1, 'Mehmet Kaplan', 'Sipariş Formu Duyuru Alanı', '2019-04-10 10:38:52', 'Mehmet');
 
 -- --------------------------------------------------------
 
@@ -131,7 +131,7 @@ INSERT INTO `siparistalepleri` (`id`, `alanid`, `urun`, `eposta`, `telefon`, `ku
 (3, '308', '3.Ürün', 'örnek@deneme.com', '05554444444', 'Mor', 'Hazır', '1', '2019-04-12 06:17:02'),
 (4, '9', '4.Ürün', 'örnek@deneme.com', '05554444444', 'Turuncu', 'Kargo', '1', '2019-04-15 11:50:13'),
 (5, '371', '5.Ürün', 'örnek@deneme.com', '05554444444', 'Sarı', 'Teslim', '1', '2019-04-15 11:51:57'),
-(22, '423', '1.Ürün', 'örnek@deneme.com', '05554444444', 'Yeşil', 'İptal', '1', '2019-04-18 11:48:33');
+(6, '511', '5.Ürün', 'örnek@deneme.com', '05554444444', 'Beyaz', 'İptal', '1', '2019-04-15 11:51:57');
 
 -- --------------------------------------------------------
 
@@ -155,8 +155,8 @@ CREATE TABLE `urunler` (
 --
 
 INSERT INTO `urunler` (`id`, `urunresim`, `urunbaslik`, `urunicerik`, `urunfiyat`, `urunstok`, `tarih`, `ekleyen`) VALUES
-(1, '1555422239.jpg', '1.Ürün', '1.Ürün İçeriği', 10, '10', '2019-04-05 17:39:56', 'Mehmet'),
-(2, '1555913237.jpg', '2.Ürün', '2.Ürün İçeriği', 23, '10', '2019-04-10 09:04:05', 'Mehmet'),
+(1, 'resimyok.jpg', '1.Ürün', '1.Ürün İçeriği', 10, '10', '2019-04-05 17:39:56', 'Mehmet'),
+(2, 'resimyok.jpg', '2.Ürün', '2.Ürün İçeriği', 23, '10', '2019-04-10 09:04:05', 'Mehmet'),
 (3, 'resimyok.jpg', '3.Ürün', '3.Ürün İçeriği', 30, '10', '2019-04-06 10:36:40', 'Mehmet'),
 (4, 'resimyok.jpg', '4.Ürün', '4.Ürün İçeriği', 40, '10', '2019-04-06 10:56:47', 'Mehmet'),
 (5, 'resimyok.jpg', '5.Ürün', '5.Ürün İçeriği', 50, '10', '2019-04-06 10:56:47', 'Mehmet');
@@ -183,7 +183,7 @@ CREATE TABLE `yoneticiler` (
 --
 
 INSERT INTO `yoneticiler` (`id`, `alanid`, `kullaniciadi`, `email`, `password`, `adiniz`, `aciklama`, `telefon`) VALUES
-(1, '', 'demo', 'demo@demo.com', 'demo', 'demo', 'Demo', 'demo');
+(2, '', 'demo', 'demo@demo.com', 'demo', 'demo', 'Demo', 'demo');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -239,19 +239,19 @@ ALTER TABLE `ayarlar`
 -- Tablo için AUTO_INCREMENT değeri `destektalepleri`
 --
 ALTER TABLE `destektalepleri`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `duyurular`
 --
 ALTER TABLE `duyurular`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `siparistalepleri`
 --
 ALTER TABLE `siparistalepleri`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `urunler`
